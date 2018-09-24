e2a759e967a5483128f36c017db2608f9ff5d61a
martaquintana <Marta@MacBook-Pro-de-Marta.local>
2018-09-24 22:28:48 +0200

    Funciona
    Funciona, cuidado con el sangriado.

--- calcoo.py
+++ calcoo.py
@@ -1,32 +1,26 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-# Clases
 import sys
 
 class Calculadora:
-
-	def plus(self, op1, op2):
-		""" Function to sum the operands. Ops have to be ints """
-		return op1 + op2
-
-	def minus(self, op1, op2):
-		""" Function to substract the operands """
-		return op1 - op2
+    def plus(self, op1, op2):
+        return op1 + op2
+		
+    def minus(self, op1, op2):
+        return op1 - op2
 
 if __name__ == "__main__":
 	micalculadora=Calculadora()
-    try:
-        operando1 = int(sys.argv[1])
-        operando2 = int(sys.argv[3])
-    except ValueError:
-        sys.exit("Error: Non numerical parameters")
+try:
+    operando1 = int(sys.argv[1])
+    operando2 = int(sys.argv[3])   
+except ValueError:
+    sys.exit("Error: Non numerical parameters")
 
-    if sys.argv[2] == "suma":
-        result = micalculadora.plus(operando1, operando2)
-    elif sys.argv[2] == "resta":
-        result = micalculadora.minus(operando1, operando2)
-    else:
-        sys.exit('Operación sólo puede ser sumar o restar.')
-
-    print(result)
-
+if sys.argv[2] == "suma": 
+	result = micalculadora.plus(operando1, operando2)
+elif sys.argv[2] == "resta":
+    result = micalculadora.minus(operando1, operando2)
+else:
+    sys.exit('Operación sólo puede ser sumar o restar.')
+print(result)
