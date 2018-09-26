76cf225a4e556c299fc64c366b80f1821f56ae7c
martaquintana <Marta@MacBook-Pro-de-Marta.local>
2018-09-26 15:47:14 +0200

    He importado y usado calcoohija y Funciona

--- calcplus.py
+++ calcplus.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys 
+import calcoohija
 
 fd = open(sys.argv[1], 'r')
 listalineas = fd.readlines()
@@ -11,29 +12,35 @@ suma=0
 resta=0
 multipli=1
 divi=1
-si=False
-print(listalineas)
+
+micalculadora=calcoohija.CalculadoraHija()
+
 for lineas in range(0,len(listalineas)):
 	linea=listalineas[lineas].split(',')
-	
 	if linea[0]=="suma":
 		for numero in range(1,len(linea)):
-			suma= int(linea[numero])+ suma
+			op1=suma
+			op2=int(linea[numero])
+			suma=micalculadora.plus(op1,op2)
 		print(suma)
 	if linea[0]=="resta":
 		resta=int(linea[1])
 		for numero in range(2,len(linea)):
-			resta=resta-int(linea[numero])
+			op1=resta
+			op2=int(linea[numero])
+			resta=micalculadora.minus(op1,op2)
 		print(resta)
 	if linea[0]=="multiplica":
 		for numero in range(1,len(linea)):
-			multipli=multipli*int(linea[numero])
+			op1=multipli
+			op2=int(linea[numero])
+			multipli=micalculadora.multipliedby(op1,op2)
 		print(multipli)
 	if linea[0]=="divide":
-		try:
 			divi=int(linea[1])
 			for numero in range(2,len(linea)):
-				divi=divi/int(linea[numero])
+				op1=divi
+				op2=int(linea[numero])
+				divi=micalculadora.dividedby(op1,op2)
 			print(int(divi))
-		except ZeroDivisionError:
-		 sys.exit("Division by zero is not allowed.")
+		
