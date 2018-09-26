095305b80b226ac2fb163b6d1b4f686d71a747bc
martaquintana <Marta@MacBook-Pro-de-Marta.local>
2018-09-26 15:09:37 +0200

    Correccion sangriado

--- calcoo.py
+++ calcoo.py
@@ -10,18 +10,19 @@ class Calculadora:
         return op1 - op2
 
 if __name__ == "__main__":
+	
 	micalculadora=Calculadora()
-try:
-    operando1 = int(sys.argv[1])
-    operando2 = int(sys.argv[3])   
-except ValueError:
-    sys.exit("Error: Non numerical parameters")
+	try:
+		operando1 = int(sys.argv[1])
+		operando2 = int(sys.argv[3])   
+	except ValueError:
+		sys.exit("Error: Non numerical parameters")
 
-if sys.argv[2] == "suma": 
-	result = micalculadora.plus(operando1, operando2)
-elif sys.argv[2] == "resta":
-    result = micalculadora.minus(operando1, operando2)
-else:
-    sys.exit('Operación sólo puede ser sumar o restar.')
+	if sys.argv[2] == "suma": 
+		result = micalculadora.plus(operando1, operando2)
+	elif sys.argv[2] == "resta":
+		result = micalculadora.minus(operando1, operando2)
+	else:
+		sys.exit('Operación sólo puede ser sumar o restar.')
     
-print(result)
+	print(result)
