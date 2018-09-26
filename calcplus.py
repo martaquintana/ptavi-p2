31cab0941c4653d06c42671da028096430b1923e
martaquintana <Marta@MacBook-Pro-de-Marta.local>
2018-09-26 17:02:22 +0200

    Aplicando correciones: guia de estilo

--- calcplus.py
+++ calcplus.py
@@ -7,40 +7,40 @@ import calcoohija
 fd = open(sys.argv[1], 'r')
 listalineas = fd.readlines()
 fd.close()
-lineasseparadas=[]
-suma=0
-resta=0
-multipli=1
-divi=1
+lineasseparadas = []
+suma = 0
+resta = 0
+multipli = 1
+divi = 1
 
-micalculadora=calcoohija.CalculadoraHija()
+micalculadora = calcoohija.CalculadoraHija()
 
 for lineas in range(0,len(listalineas)):
 	linea=listalineas[lineas].split(',')
 	if linea[0]=="suma":
 		for numero in range(1,len(linea)):
-			op1=suma
-			op2=int(linea[numero])
-			suma=micalculadora.plus(op1,op2)
+			op1 = suma
+			op2 = int(linea[numero])
+			suma = micalculadora.plus(op1,op2)
 		print(suma)
 	if linea[0]=="resta":
-		resta=int(linea[1])
+		resta = int(linea[1])
 		for numero in range(2,len(linea)):
-			op1=resta
-			op2=int(linea[numero])
-			resta=micalculadora.minus(op1,op2)
+			op1 = resta
+			op2 = int(linea[numero])
+			resta = micalculadora.minus(op1,op2)
 		print(resta)
 	if linea[0]=="multiplica":
 		for numero in range(1,len(linea)):
-			op1=multipli
-			op2=int(linea[numero])
-			multipli=micalculadora.multipliedby(op1,op2)
+			op1 = multipli
+			op2 = int(linea[numero])
+			multipli = micalculadora.multipliedby(op1,op2)
 		print(multipli)
 	if linea[0]=="divide":
-			divi=int(linea[1])
+			divi = int(linea[1])
 			for numero in range(2,len(linea)):
-				op1=divi
-				op2=int(linea[numero])
-				divi=micalculadora.dividedby(op1,op2)
+				op1 = divi
+				op2 = int(linea[numero])
+				divi = micalculadora.dividedby(op1,op2)
 			print(int(divi))
 		
