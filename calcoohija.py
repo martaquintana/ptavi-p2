ddcda0b2b132103d2ee609d08e42566ed5e27912
martaquintana <Marta@MacBook-Pro-de-Marta.local>
2018-09-26 15:08:32 +0200

    He importado calcoo y lo he utilizado
    Así me ahorro las lineas de suma y resta.

--- calcoohija.py
+++ calcoohija.py
@@ -1,43 +1,36 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import sys
+import calcoo  #Utilizando el programa anterior, calcoo, no hace falta poner la suma ni la resta ya lo sabe el programa
 
-class Calculadora:
-    def plus(self, op1, op2):
-        return op1 + op2
-		
-    def minus(self, op1, op2):
-        return op1 - op2
-        
-class CalculadoraHija(Calculadora):
+class CalculadoraHija(calcoo.Calculadora):
     def multipliedby(self, op1, op2):
         return op1 * op2
         
     def dividedby(self, op1, op2):
-        if op2 != 0:
+        try:
             return op1 / op2
-        else:
-            print ["Division by zero is not allowed."]
+        except ZeroDivisionError:
+            sys.exit("Division by zero is not allowed.")
 
 
 
 if __name__ == "__main__":
-	micalculadora=Calculadora()
-	micalculadorahija=CalculadoraHija()
-try:
-    operando1 = int(sys.argv[1])
-    operando2 = int(sys.argv[3])   
-except ValueError:
-    sys.exit("Error: Non numerical parameters")
+	micalculadora = CalculadoraHija()
+	try:
+		operando1 = int(sys.argv[1])
+		operando2 = int(sys.argv[3])   
+	except ValueError:
+		sys.exit("Error: Non numerical parameters")
 
-if sys.argv[2] == "suma": 
-	result = micalculadora.plus(operando1, operando2)
-elif sys.argv[2] == "resta":
-    result = micalculadora.minus(operando1, operando2)
-elif sys.argv[2] == "multiplica":
-    result = micalculadorahija.multipliedby(operando1, operando2)
-elif sys.argv[2] == "divide":
-    result = micalculadorahija.dividedby(operando1, operando2)
-else:
-    sys.exit('Operación sólo puede ser suma, resta, multiplica o divide.')
-print(result)
+	if sys.argv[2] == "suma": 
+		result = micalculadora.plus(operando1, operando2)
+	elif sys.argv[2] == "resta":
+		result = micalculadora.minus(operando1, operando2)
+	elif sys.argv[2] == "multiplica":
+		result = micalculadora.multipliedby(operando1, operando2)
+	elif sys.argv[2] == "divide":
+		result = micalculadora.dividedby(operando1, operando2)
+	else:
+		sys.exit('Operación sólo puede ser suma, resta, multiplica o divide.')
+	print(result)
